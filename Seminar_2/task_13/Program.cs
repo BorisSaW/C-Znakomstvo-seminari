a9b03570ef7669c45526f5a5d864bfbbd7b3d17c
Boris <burbon1821@yandex.ru>
2022-08-29 16:41:46 +0300

    Added Seminar 2 and done homework

---
+++ Seminar_2/task_13/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру (справа налево) заданного числа 
+//или сообщает, что третьей цифры нет.
+// 645 -> 6
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+Console.WriteLine("Введите число ");
+int number = int.Parse(Console.ReadLine());
+
+int a1 = number / 100;
+int a2 = a1 % 10;
+
+int result = a2;
+
+if(number >= 100)
+{
+    Console.WriteLine($"{number} -> {result}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
---
+++ Seminar_2/task_15/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+if (dayNumber == 6 || dayNumber == 7) 
+  {
+  Console.WriteLine("(выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) 
+  {
+    Console.WriteLine("не является днем недели");
+  }
+  else 
+  Console.WriteLine("(не выходной) -> нет");
